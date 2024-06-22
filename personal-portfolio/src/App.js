ecdc317db4ebf01b23f4adf6dfe7595f830fefd4
asidhu0 <asidhu359@gmail.com>
2024-06-22 14:44:33 -0700

    removed build from gitignore

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -4,31 +4,31 @@ import NavBar from './NavBar'; // Make sure the path is correct
 import HomePage from './HomePage';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <NavBar />
-//         <HomePage />
-//       </header>
-//     </div>
-//   );
-// }
-
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
-      <Switch>
-        <Route exact path="/" component={Home} />
-          <div className="App">
-            <header className="App-header">
-              <NavBar />
-              <HomePage />
-            </header>
-        </div>
-      </Switch>
-    </Router>
+    <div className="App">
+      <header className="App-header">
+        <NavBar />
+        <HomePage />
+      </header>
+    </div>
   );
 }
+
+// function App() {
+//   return (
+//     <Router basename={process.env.PUBLIC_URL}>
+//       <Switch>
+//         <Route exact path="/" component={Home} />
+//           <div className="App">
+//             <header className="App-header">
+//               <NavBar />
+//               <HomePage />
+//             </header>
+//         </div>
+//       </Switch>
+//     </Router>
+//   );
+// }
 
 export default App;
