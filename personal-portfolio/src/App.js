e21323285a6a05fe69ba3fd7a0886b181c52e5f4
asidhu0 <asidhu359@gmail.com>
2024-06-22 14:48:19 -0700

    removed error

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,7 +2,6 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar'; // Make sure the path is correct
 import HomePage from './HomePage';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
@@ -14,21 +13,5 @@ function App() {
     </div>
   );
 }
-
-// function App() {
-//   return (
-//     <Router basename={process.env.PUBLIC_URL}>
-//       <Switch>
-//         <Route exact path="/" component={Home} />
-//           <div className="App">
-//             <header className="App-header">
-//               <NavBar />
-//               <HomePage />
-//             </header>
-//         </div>
-//       </Switch>
-//     </Router>
-//   );
-// }
 
 export default App;
