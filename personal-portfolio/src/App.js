6932b828381a53f715c789095e2be7e910555e58
asidhu0 <asidhu359@gmail.com>
2024-06-22 13:34:21 -0700

    add routing

--- personal-portfolio/src/App.js
+++ personal-portfolio/src/App.js
@@ -2,15 +2,32 @@ import logo from './logo.svg';
 import './App.css';
 import NavBar from './NavBar'; // Make sure the path is correct
 import HomePage from './HomePage';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <NavBar />
+//         <HomePage />
+//       </header>
+//     </div>
+//   );
+// }
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <NavBar />
-        <HomePage />
-      </header>
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Switch>
+        <Route exact path="/" component={Home} />
+          <div className="App">
+            <header className="App-header">
+              <NavBar />
+              <HomePage />
+            </header>
+        </div>
+      </Switch>
+    </Router>
   );
 }
 
